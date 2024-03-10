2008cd10509edf034166e2e1ff705fb6717f9e82
xiao <cangzhishang@gmail.com>
2024-03-09 19:04:31 -0600

    day 22 add app description, image, and links

--- main.py
+++ main.py
@@ -22,15 +22,24 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content)
 
-col3,col4= st.columns(2)
+col3,empty_col,col4= st.columns([1.5,0.5,1.5])
 
 df=pandas.read_csv('data.csv',sep=';')
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+row['image'])
+        # st.write('[Source Code](http://www.pythonhow.com)')
+        st.write(f"[Source Code]({row['url']})")
+
 
 
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        # st.write('[Source Code](http://www.pythonhow.com)')
+        st.write(f"[Source Code]({row['url']})")
